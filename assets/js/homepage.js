51e7ff119f805699ad78e2b315859da6a9a68ecc
xtrannguyen <tnguyenmw@gmail.com>
2021-04-07 07:38:29 -0400

    updated function getuserrepos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,13 @@
-var getUserRepos = function() {
-    var response = fetch("https://api.github.com/users/octocat/repos")
-            .then(function(response) {
-               response.json()
-            .then(function(data){
-                console.log(data);
-            }); 
-            });
-
-    console.log("outside");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
   };
   
   getUserRepos();
